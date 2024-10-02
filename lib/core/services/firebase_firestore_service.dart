ae605522e91af532beabf9cdc5a19f714328ac8f
Mohammed Adil <66908713+mohammed-xp@users.noreply.github.com>
2024-10-02 15:50:00 +0200

    Implement fetch data

--- lib/core/services/firebase_firestore_service.dart
+++ lib/core/services/firebase_firestore_service.dart
@@ -9,4 +9,13 @@ class FirebaseFirestoreService implements DatabaseService {
       {required String path, required Map<String, dynamic> data}) async {
     await FirebaseFirestore.instance.collection(path).add(data);
   }
+
+  @override
+  Future<Map<String, dynamic>> getData(
+      {required String path, required String id}) async {
+    var result =
+        await FirebaseFirestore.instance.collection(path).doc(id).get();
+
+    return result.data() as Map<String, dynamic>;
+  }
 }
--- lib/features/auth/data/models/user_model.dart
+++ lib/features/auth/data/models/user_model.dart
@@ -15,4 +15,12 @@ class UserModel extends UserEntity {
       userId: user.uid,
     );
   }
+
+  factory UserModel.fromJson(Map<String, dynamic> json) {
+    return UserModel(
+      name: json['name'],
+      email: json['email'],
+      userId: json['userId'],
+    );
+  }
 }
